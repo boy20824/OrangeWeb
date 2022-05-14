b9b99809520ff072c642f5d88d5ddf9df9ecb84b
JohnTsai <boy20824@gmail.com>
2022-05-14 12:49:59 +0800

    spring security先不過濾(後需再更新),修正 shop的pom 不要引入generator子包

---
+++ shop/src/main/java/com/orange/web/shop/config/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.orange.web.shop.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true) //開啟註解權限管理
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable().authorizeRequests().antMatchers("/").permitAll();
+    }
+}
